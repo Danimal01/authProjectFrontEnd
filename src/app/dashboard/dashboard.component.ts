70d36c49f0d04d0a4ff5ba0014eb70f93419167f
Danimal01 <danialdadkhoo@gmail.com>
2023-07-14 15:48:05 -0400

    worksButHasMMNonceIssue

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core'; // <- add OnInit import here
 import { DashboardService } from '../services/dashboard.service';
+import { Router } from '@angular/router';  // <-- import Router here
+import { HttpClient } from '@angular/common/http';
 
+
+declare let window: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -8,16 +12,62 @@ import { DashboardService } from '../services/dashboard.service';
   styleUrls: ['./dashboard.component.less']
 })
 export class DashboardComponent implements OnInit { // Implement OnInit interface
-  userEmail: string = ''; // Initialize userEmail with an empty string
+  userEmail: string = '';
+  address: string | null = null;
+  balance: string | null = null;
 
-  constructor(private dashboardService: DashboardService) { }
+  constructor(private dashboardService: DashboardService, private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
     // Use '|| '' to provide a default value of '' if localStorage does not contain 'email'
     this.userEmail = localStorage.getItem('email') || '';
   }
 
-  getWelcomeMessage() {
+  logout() {
+    localStorage.removeItem('email');
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 
+  async connectMetamask() {
+    if (window.ethereum) {
+      try {
+        // Request account access
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        // After getting the permission
+        // Fetching the Wallet Balance
+        const balance = await window.ethereum.request({
+          method: 'eth_getBalance',
+          params: [accounts[0], 'latest']
+        });
+
+        // Update state with address and balance
+        this.address = accounts[0];
+        this.balance = this.fromWei(balance);
+
+        // The object that we will send to the backend
+        const userData = {
+          email: this.userEmail,
+          ethereumAddress: this.address
+        };
+
+        // Send a POST request to the backend with the user's email and Ethereum address
+        this.http.post('http://localhost:8080/api/connect-metamask', userData).subscribe({
+          next: (response) => console.log('Updated user data:', response),
+          error: (err) => console.error('Failed to update user data:', err)
+        });
+
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+
+  fromWei(wei: string): string {
+    return (parseInt(wei) / 1e18).toString();
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+declare let window: any;
+import Web3 from 'web3';
+
 
 @Component({
   selector: 'app-login',
@@ -31,5 +34,59 @@ export class LoginComponent {
       }
     );
   }
+
+  async loginWithMetamask() {
+    try {
+      // Request account access
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+      // Get the first account
+      const account = accounts[0];
+
+
+      // Send a request to the backend to create a challenge for this account
+      this.http.post('http://localhost:8080/api/challenge', { ethereum_address: account }).subscribe({
+        next: async (response: any) => {
+          const nonceString = response.nonce.toString();
+          console.log(`Received nonce: ${nonceString}`);  // Add this log statement here
+
+          const message = `\x19Ethereum Signed Message:\n${nonceString.length}${nonceString}`;
+
+
+
+          console.log(`Signing message: ${message}`); // Add this log here
+
+
+
+          // Sign the challenge with the user's private key
+          const signature = await window.ethereum.request({
+            method: 'personal_sign',
+            params: [message, account],
+          });
+
+          console.log(`Signature: ${signature}`); // Add this log here
+
+
+          // Send the signature to the backend to verify and log in
+          this.http.post('http://localhost:8080/api/login-metamask', { ethereum_address: account, signature }).subscribe({
+            next: (response: any) => {
+              console.log('Logged in:', response);
+
+              localStorage.setItem('token', response.token);
+              // Assuming you are using Angular routing and you have a route for '/dashboard'
+              this.router.navigate(['/dashboard']);
+
+            },
+
+            error: (err) => console.error('Failed to create challenge:', err)
+          });
+        },
+        error: (err) => console.error('Failed to create challenge:', err)
+      });
+    } catch (error) {
+      console.error('Failed to connect Metamask:', error);
+    }
+  }
+
 
 }
